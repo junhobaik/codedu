ff5d99c6c614f348a8003231da2221ca250e9de8
HoKyeong Eom <latilt2@gmail.com>
2017-04-26 02:25:59 +0900

    [MODIFY] QuizeResultDesign
    퀴즈결과화면 레이아웃 디자인

--- codedu/src/components/QuizResult/index.js
+++ codedu/src/components/QuizResult/index.js
@@ -1,10 +1,41 @@
 import React, { Component, PropTypes } from 'react';
+import { Button, Progress } from 'semantic-ui-react';
+import './result.css';
 
 class QuizResult extends Component {
     render() {
+        const rightAnswer = 3;
+        const totalAnswer = 10;
+        const userLevel = 5;
+        const userExp = 30;
+        const gainExp = 10;
+        const totalExp = userExp + gainExp;
+
         return (
-            <div>
-                QuizResult
+            <div className="result-wrap">
+                <div className="result-header">
+                    <span className="mile">
+                        PART1 > BASIC
+                    </span>
+                    <a href="/"><Button floated="right" className="result-button-out">나가기</Button></a>
+                </div>
+                <div className="result-content">
+                    <div className="result-exp">
+                        <div className="result-level">
+                            Lv. {userLevel}
+                        </div>
+                        <Progress percent={totalExp} style={{margin: 0}}/>
+                        <div className="result-up">
+                            + {gainExp}%
+                        </div>
+                    </div>
+
+                    <div className="result-message">
+                        <p>정답 : {rightAnswer} / {totalAnswer}</p>
+
+                        <p>다시 공부하세요</p>
+                    </div>
+                </div>
             </div>
         );
     }
