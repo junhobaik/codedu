f4dc2257f29694f5c8cc818298b140e2fe5b57bc
HoKyeong Eom <latilt2@gmail.com>
2017-04-25 17:00:03 +0900

    [MODIFY] LoginButton Modal
    로그인버튼 클릭 시 로그인모달레이어 팝업

--- codedu/src/components/Common/LoginButton/LoginButton.js
+++ codedu/src/components/Common/LoginButton/LoginButton.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Button, Modal} from 'semantic-ui-react';
 
 class LoginButton extends Component {
   render() {
     return (
-        <div className="login">Login</div>
+        <Modal trigger={<Button className="login">Login</Button>}>
+          <Modal.Content>
+            <p>Login Modal</p>
+          </Modal.Content>
+        </Modal>
     );
   }
 }
