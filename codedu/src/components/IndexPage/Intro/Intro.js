ada4e88dffbb473e2262f466befa56e4f247000a
junhobaik <junhobaik@gmail.com>
2017-04-25 17:30:23 +0900

    [ADD] index page design
    - 인덱스 페이지 레이아웃 디자인 작업

--- codedu/src/components/IndexPage/Intro/Intro.js
+++ codedu/src/components/IndexPage/Intro/Intro.js
@@ -1,10 +1,13 @@
+import faker from 'faker';
 import React, { Component, PropTypes } from 'react';
 
 class Intro extends Component {
     render() {
         return (
             <div className="intro-wrap">
-                intro     
+                <div className="space"></div>
+                <img src={faker.image.imageUrl()} alt="intro-img"/>
+                <div className="space"></div>
             </div>
         );
     }
--- codedu/src/components/IndexPage/Join/Join.js
+++ codedu/src/components/IndexPage/Join/Join.js
@@ -1,10 +1,32 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Button, Form, Input, Message, Icon } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css';
+
 class Join extends Component {
     render() {
         return (
             <div className="join-wrap">
-                Join      
+                <div className="join-intro">
+                    &lt;Code&gt;du 와 함께 <br/>
+                    학습하시려면 가입해주세요.
+                </div>
+                <hr/>
+                <Form>
+                    <Form.Input iconPosition='left' label='Email' placeholder='이메일 주소' >
+                        <Icon name='at'/>
+                        <input/>
+                    </Form.Input>
+                    <Form.Input iconPosition='left' label="Password" name="password" type="password" placeholder='비밀번호'>
+                        <Icon name='lock'/>
+                        <input/>
+                    </Form.Input>
+                    <Form.Input iconPosition='left' label="Vertify password" name="password" type="password" placeholder='비밀번호 확인'>
+                        <Icon name='lock'/>
+                        <input/>
+                    </Form.Input>
+                    <Button className='submit' fluid type='submit'>가입하기</Button>
+                </Form>
             </div>
         );
     }
