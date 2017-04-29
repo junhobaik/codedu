5531eb834aad4243c2d83dda558bcadc4f4568a3
HoKyeong Eom <latilt2@gmail.com>
2017-04-30 01:18:55 +0900

    [ADD] Route '/api/logout'

--- codedu/server/routes/api/index.js
+++ codedu/server/routes/api/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const user = require('./user/index')
 const join = require('./join/index')
+const logout = require('./logout/index')
 
 router.use('/user', user)
 router.use('/join', join)
+router.use('/logout', logout)
 
 module.exports = router
