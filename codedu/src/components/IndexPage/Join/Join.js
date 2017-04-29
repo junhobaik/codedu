620ebc6d9942553d61107accd8ebf131fe7ed84d
junhobaik <junhobaik@gmail.com>
2017-04-30 00:47:27 +0900

    [MODIFY] Join in index page
    - 프로젝트에 맞게 join 코드 수정

--- codedu/src/components/IndexPage/Join/Join.js
+++ codedu/src/components/IndexPage/Join/Join.js
@@ -1,8 +1,55 @@
 import React, { Component, PropTypes } from 'react';
+import {browserHistory} from 'react-router';
 
 import { Button, Form, Input, Message, Icon } from 'semantic-ui-react'
 
 class Join extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            isLogin: false,
+            message: null,
+            userName: null
+        }
+    }
+
+    join = (evt) => {
+        evt.preventDefault();
+        const form = evt.target.parentElement;
+        fetch('/api/join', {
+                method: 'POST',
+                dataType: 'json',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: form[0].value,
+                    password: form[1].value,
+                    vpassword: form[2].value,
+                })
+            })
+            .then((response) => {
+                console.log("response", response);
+                if (response.redirected === true) {
+                    const path = response.url.split('/')[3];
+                    console.log("local-join Strategy success, move to /", path);
+                    browserHistory.push(path);
+                } else {
+                    return response.json();
+                }
+                return response.json();
+            })
+            .then((responseData) => {
+                console.log("responseData",responseData);
+                this.setState({
+                    message: responseData.message
+                });
+            })
+            .catch((error) => {
+                //console.log('Error Fetch', error);
+            })
+    }
     render() {
         return (
             <div className="join-wrap">
@@ -11,7 +58,7 @@ class Join extends Component {
                     학습하시려면 가입해주세요.
                 </div>
                 <hr/>
-                <Form action="/api/join" method="post">
+                <Form action='/api/join' method='post'>
                     <Form.Input name='email' iconPosition='left' label='Email' placeholder='이메일 주소' >
                         <Icon name='at'/>
                         <input/>
@@ -24,8 +71,10 @@ class Join extends Component {
                         <Icon name='lock'/>
                         <input/>
                     </Form.Input>
-                    <Button className='submit' fluid type='submit'>가입하기</Button>
+                    {/*<Button onClick={this.join} className='submit' fluid type='submit'>가입하기</Button>*/}
+                    <Button onClick={this.join} className='submit' fluid >가입하기</Button>
                 </Form>
+                <div className='messege'>{this.state.message}</div>
             </div>
         );
     }
