5d29d31e72324e02181466d746fcb09b4a80dac0
junhobaik <junhobaik@gmail.com>
2017-04-30 18:34:29 +0900

    [UPDATE] index Page join part
    - 이메일 형식 체크, 패스워드 글자 제한 추가

--- codedu/server/routes/api/join/index.js
+++ codedu/server/routes/api/join/index.js
@@ -76,27 +76,38 @@ passport.use('local-join', new LocalStrategy({
 router.post('/', function (req, res, next) {
     const data = req.body
     console.log("data=", data);
+    const passwordMinLength = 7;
+    const passwordMaxLength = 16;
     if (data.email === '' || data.password === '' || data.vpassword === '') {
       res.json({
         'message': "입력되지 않은 값이 있습니다"
       });
-    } else {
-      var query = connection.query('select * from user where email=?', [data.email], function (err, rows) {
-        if (err) throw err;
-        else if (rows.length) {
-          res.json({
-            'message': "이미 가입된 이메일입니다"
-          });
-        } else if (data.password !== data.vpassword) {
-          res.json({
-            'message': "패스워드 확인이 틀립니다"
-          })
-        } else {
-          next();
-        }
-      })
-    }
-  },
+      }
+      else if (data.email.indexOf('@') === -1 || data.email.indexOf('.') === -1) {
+        res.json({
+          'message': "이메일을 양식에 맞게 작성해주세요"
+        });
+      } else if (data.password.length < passwordMinLength || data.password.length > passwordMaxLength) {
+        res.json({
+          'message': "패스워드 길이는 '7자 이상 & 16자 이하' 입니다"
+        });
+      } else if (data.password !== data.vpassword) {
+        res.json({
+          'message': "패스워드 확인이 틀립니다"
+        })
+      } else {
+        var query = connection.query('select * from user where email=?', [data.email], function (err, rows) {
+          if (err) throw err;
+          else if (rows.length) {
+            res.json({
+              'message': "이미 가입된 이메일입니다"
+            });
+          } else {
+            next();
+          }
+        })
+      }
+      },
   passport.authenticate('local-join', {
     successRedirect: '/main', //성공시 리다이렉트
     failureRedirect: '/', //실패시 리다이렉트
