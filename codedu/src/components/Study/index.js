d1dbccfbaca517dcdd0efe6490b678bfae22f123
HoKyeong Eom <latilt2@gmail.com>
2017-05-04 17:12:13 +0900

    [MODIFY] Study Component
    라우팅에 따라 데이터를 가져올수 있도록 fetch 추가

--- codedu/src/components/Study/index.js
+++ codedu/src/components/Study/index.js
@@ -3,17 +3,51 @@ import { Button } from 'semantic-ui-react';
 import './study.css';
 
 class StudyMaterial extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: null,
+            content: null
+        }
+    }
+    getData = () => {
+        fetch('/api'+window.location.pathname, {
+            method: "get",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin'
+        })
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .then((responseData) => {
+            console.log(responseData);
+            this.setState({title: responseData.quiz_title, content: responseData.quiz_content})
+        })
+        .catch((error) => {
+            console.log('fetch error', error);
+        })
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
     render() {
+
         return (
             <div className="study-wrap">
                 <div className="study-header">
                     <span className="mile">
-                        PART1 > BASIC
+                        PART1 > {this.state.title}
                     </span>
                     <Button floated='right' className="study-start-button top">시작</Button>
                 </div>
                 <div className="study-content">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates pariatur nisi facilis ullam expedita, in dolor tempore ab cumque amet culpa adipisci sunt dolorem dolorum optio ipsum cupiditate magnam quidem.
+                    {this.state.content}
                 </div>
                 <Button fluid className="study-start-button bottom">시작</Button>
             </div>
