e78e2f6d8993336c983cf288cdb6217c47c8fdac
HoKyeong Eom <latilt2@gmail.com>
2017-05-09 22:49:20 +0900

    [ADD] Quiz, Problems

--- codedu/model/part.js
+++ codedu/model/part.js
@@ -15,18 +15,17 @@ const quizSchema = new Schema({
 module.exports = mongoose.model('part', partSchema)
 
 // {
-//   part_title: 'Javascirpt Basic of Basic',
+//   part_title: 'Javascript Basic of Basic',
 //   quiz: [
 //     {
-//       order: 0,
 //       quiz_title: 'First Step',
 //       quiz_content: 'quiz1.md',
-//       problems: [{prob:'pro1.md', ans:3}, 'pro2.md', 'pro3.md', 'pro4.md', 'pro5.md']
+//       problems: 'problem1.json'
 //     },
 //     {
 //       quiz_title: 'Variable',
 //       quiz_content: 'quiz2.md',
-//       problems: ['pro1.md', 'pro2.md', 'pro3.md', 'pro4.md', 'pro5.md']
+//       problems: 'problem2.json'
 //     }
 //   ]
 // }
