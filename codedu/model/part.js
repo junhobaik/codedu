8546ceb6957681c768fedbd1b9238d7f5db54a3d
HoKyeong Eom <latilt2@gmail.com>
2017-05-10 11:14:43 +0900

    [ADD] Server /api/quiz
    Connect Mongodb, find quiz_title, send problem.json

--- codedu/model/part.js
+++ codedu/model/part.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const partSchema = new Schema({
-  part_title: String,
-  quiz: [quizSchema] 
-})
-
 const quizSchema = new Schema({
   quiz_title: String,
   quiz_content: String,
   problems: [String]
 })
+
+const partSchema = new Schema({
+  part_title: String,
+  quiz: [quizSchema] 
+})
+
+
 
 module.exports = mongoose.model('part', partSchema)
 
