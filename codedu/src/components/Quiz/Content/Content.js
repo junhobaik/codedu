309495ac1a5e368c0fba851869d0e9028aa11af8
junhobaik <junhobaik@gmail.com>
2017-05-16 19:32:45 +0900

    [MODIFY] QuizPage content, use json2md

--- codedu/src/components/Quiz/Content/Content.js
+++ codedu/src/components/Quiz/Content/Content.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import json2md from 'json2md';
+import ReactMarkdown from 'react-markdown';
 
 class Content extends Component {
 	render() {
 		const { number, problem, checkAnswer } = this.props;
 		let content = null;
 		let items = null;
+
 		if (problem) {
 			content = problem[number].content;
+			content = json2md(content);
 			items = problem[number].items.map((value, index) => {
-				return (<li className='items' key={index} onClick={checkAnswer.bind(this, index)}>{value}</li>)
+				return (<li className='items' refs={index} key={index} onClick={checkAnswer.bind(this, index)}>{value}</li>)
 			});
 		}
 
 		return (
 			<div className='content-wrap'>
 				<div className='problem'>
-					{content}
+					<ReactMarkdown className="markdown" source={content} />
 				</div>
 				<div className='space'></div>
 				<ul>
