7368b02e6d6549c6261fb5abf2475e0342ac3be1
zeevi6 <gwonhong.ord@gmail.com>
2017-05-16 13:01:13 +0900

    [MODIFY] mark done/not-done with css - test button should be modified later

--- codedu/src/components/Main/Quiz/index.js
+++ codedu/src/components/Main/Quiz/index.js
@@ -4,13 +4,29 @@ import {Link} from 'react-router';
 
 
 class Quiz extends Component {
+    state = {
+        is_quiz_done: false
+    }
+
+    componentWillMount() {
+        const progress = this.props.progress
+        const quiz_title = this.props.quiz.quiz_title
+        if(progress.indexOf(quiz_title) > -1) {
+            this.setState({
+                is_quiz_done: true
+            })
+        }
+    }
+
     render() {
         console.log("Quiz title is",this.props.title);
         console.log("Quiz quiz is",this.props.quiz);
-        
+
         return (
             <div className="quiz-button">
-                <Link to={"/study/"+this.props.title+"&"+this.props.quiz.quiz_title}><Button>{this.props.quiz.quiz_title}</Button></Link>
+                <Link to={"/study/"+this.props.title+"&"+this.props.quiz.quiz_title}>
+                    <Button className={this.state.is_quiz_done? 'quiz-done' : 'quiz-not-done'}>{this.props.quiz.quiz_title}</Button>
+                </Link>
             </div>
         );
     }
