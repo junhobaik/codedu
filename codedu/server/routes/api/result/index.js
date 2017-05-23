35fea86d022e4259806250cf891a9290ad4f261c
zeevi6 <gwonhong.ord@gmail.com>
2017-05-23 14:44:44 +0900

    [FIX] update days_of_week when quiz passed

--- codedu/server/routes/api/result/index.js
+++ codedu/server/routes/api/result/index.js
@@ -15,9 +15,20 @@ router.post('/', function(req, res) {
     if(err) return err
 
     if(rows) {
-      const currentExp = rows[0].exp
+      const currentExp = rows[0].exp;
       const totalExp = currentExp + score * 5;
-      const update = connection.query('UPDATE user set exp = ? where email = ?', [totalExp, email], function(err, rows) {
+      let daysOfWeek = rows[0].days_of_week;  // 월화수목금토일
+      let today = new Date().getDay();  // 일월화수목금토 index: 0,1,2,3,4,5,6
+      today--;
+      
+      if(today < 0) {
+        today = 6;
+      }
+
+      daysOfWeek = daysOfWeek.substr(0, today) + 'Y' + daysOfWeek.substr(today + 1);
+      console.log(daysOfWeek);
+
+      const update = connection.query('UPDATE user set exp = ?, days_of_week = ? where email = ?', [totalExp, daysOfWeek, email], function(err, rows) {
         if(err) return err
 
         res.json({result: "ok", exp: totalExp})
