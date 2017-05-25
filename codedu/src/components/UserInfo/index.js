4c94fd45e292243a402550296d13177ca0db4df7
junhobaik <junhobaik@gmail.com>
2017-05-25 14:25:47 +0900

    [MODIFY] UserInfo Page Style modify

--- codedu/src/components/UserInfo/index.js
+++ codedu/src/components/UserInfo/index.js
@@ -8,13 +8,17 @@ class UserInfo extends Component {
     render() {
 
         return (
-            <div className='user-info-wrap'>
-                <div>
-                    <h3>설정 > 회원정보수정</h3>
-                    <UserInfoSubmit />
-                </div>
-                <p></p>
-                <UserInfoForm />
+            <div className="user-info-outter-wrap">
+                <div className="sapce"></div>
+                    <div className='user-info-wrap'>
+                        <div>
+                            <h3>설정 > 회원정보수정</h3>
+                            <UserInfoSubmit />
+                        </div>
+                        <p></p>
+                        <UserInfoForm />
+                    </div>
+                <div className="sapce"></div>
             </div>
         );
     }
